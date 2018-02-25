3ab59d9dfebf3b695ea0d486d3251a9a37857f23
MarinaRH <l_marina1717@hotmail.com>
2018-02-25 12:20:47 -0500

    Agregando section de search

--- public/js/app.js
+++ public/js/app.js
@@ -18,28 +18,34 @@ function getFetch() {
   for (let i = 1; i < 60; i++) {
 
     inputSearch.addEventListener('keydown',function(){
-        let search = inputSearch.value;
-        var container = document.getElementById('home');
-  
-        const url = `https://api.mercadolibre.com/sites/MPE/search?condition=new&q=${search}`;
-        fetch(url)
-          .then((resp) => resp.json())
-          .then(function (data) {
-  
-            console.log(data.results[i]);
-            const characters = `<div class="card" >
-                  <img id="imgg" class="card-img-top img-characters" data-toggle="modal" data-target="#MyModal" src="${data.results[i].thumbnail}" alt="${data.name}">
-                  <div class="card-body">
-                    <h5 class="card-title weight text-center">${data.results[i].title}</h5>
-                    <p>S/.${data.results[i].price}</p>
-                  </div>
-                </div>`;
-  
-            container.innerHTML += characters;
-          })
-          .catch(function (error) {
-            console.log(JSON.stringify(`Error ${error}`));
-          });
+      let search = inputSearch.value;
+      var container = document.getElementById('section');
+      container.innerHTML='';
+      const url = `https://api.mercadolibre.com/sites/MPE/search?condition=new&q=${search}`;
+      fetch(url)
+        .then((resp) => resp.json())
+        .then(function (data) {
+
+          console.log(data.results[i]);
+          const characters = 
+          `<div class="row">
+            <div class="col-4 col-md-3 col offset-md-2">
+            <img id="imgg" class="img-characters" data-toggle="modal" data-target="#MyModal" src="${data.results[i].thumbnail}" alt="${data.name}">
+            </div>
+            <div class="col-8 col-md-6">
+              <h6>${data.results[i].title}</h6>
+              <h5>S/.${data.results[i].price}</h5>
+              <span><i class="fa fa-truck"></i> Envio a todo el País<span>
+              <p> ${data.results[i].sold_quantity}  vendidos-Lima</p>
+              <button type="button" class="btn btn-warning">Comprar</button>
+            </div>
+          </div><hr>`;
+
+          container.innerHTML += characters;
+        })
+        .catch(function (error) {
+          console.log(JSON.stringify(`Error ${error}`));
+        });
   
     })
   
